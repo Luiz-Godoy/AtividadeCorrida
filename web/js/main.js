a9f4229ac26f2239c477f764fddca62aa21570a1
Luiz-Godoy <luiz.godoy255@gmail.com>
2022-08-23 14:35:58 -0300

    Atualizado!

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,12 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/service-worker-minimum-to-intall-pwa.js')
-        .then(function(registration) {
-            alert('Registration successful, scope is:', registration.scope);
+    console.log("main.js => Vamos registrar o service worker!!!");
+    navigator.serviceWorker.register('/service-worker-minimum-to-intall-pwa.js')
+        .then(registration => {
+            console.log("main.js => Service Worker Registrado com Sucesso");
+            console.dir(registration);
         })
-        .catch(function(error) {
-            alert('Service worker registration failed, error:', error);
+        .catch(error => {
+            console.log("main.js => Erro ao registrar Service Worker");
+            console.dir(error);
         });
 }
