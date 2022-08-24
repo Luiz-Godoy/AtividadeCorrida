11598ae48518c56c185fcd1ff3ce688a075b2299
Luiz-Godoy <107517245+Luiz-Godoy@users.noreply.github.com>
2022-08-24 19:26:51 -0300

    Update Jockey.java

--- CorridaCavalos/lib/src/main/java/CorridaCavalos/Jockey.java
+++ CorridaCavalos/lib/src/main/java/CorridaCavalos/Jockey.java
@@ -10,43 +10,4 @@ public class Jockey <string> {
 		this.medicos = e;
 		this.jockeys = j;
 	}
-	
-	public int forca() {
-		return this.cavalo.forca();
-	}
-	
-	public int aceleracao() {
-		return this.cavalo.aceleracao();
-	}
-	
-	public int velocidadeMaxima() {
-		return this.cavalo.velocidadeMaxima();
-	}
-	
-	public int resistencia() {
-		return this.cavalo.resistencia();
-	}
-	
-	public int idade() {
-		return this.cavalo.idade();
-	}
-	
-	@SuppressWarnings("unchecked")
-	public string raca() {
-		return (string) this.cavalo.raca();
-	}
-	
-	@SuppressWarnings("unchecked")
-	public string nome() {
-		return (string) this.medicos.nome();
-	}
-	
-	@SuppressWarnings("unchecked")
-	public string sexo() {
-		return (string) this.medicos.sexo();
-	}
-	
-	public int NivelHabilidade() {
-		return this.medicos.NivelHabilidade();
-	}
 }
